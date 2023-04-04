58738009b9ee726ab8fe4842c622505982dd2bd3
moodong1250 <dydy8899@naver.com>
2023-04-04 20:20:20 +0900

    AutoLayout_test

---
+++ AutiLayout_test/AutiLayout_test/AutiLayout_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutiLayout_testApp.swift
+//  AutiLayout_test
+//
+//  Created by 엄요준 on 2023/04/04.
+//
+
+import SwiftUI
+
+@main
+struct AutiLayout_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutoLayout_test1/AutoLayout_test1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayout_test1
+//
+//  Created by 엄요준 on 2023/04/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
